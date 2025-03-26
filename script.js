ce8091e974b30c179c9bf43925118c727a30e825
Chandan Chaudhary <166887599+chandan9648@users.noreply.github.com>
2025-03-26 14:01:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector("button")
+let box = document.querySelector("#box")
+btn.addEventListener('click', function(){
+    let R = Math.floor(Math.random()*256) 
+    let G = Math.floor(Math.random()*256)
+    let B = Math.floor(Math.random()*256)
+
+    box.style.backgroundColor = `rgb(${R},${G},${B})`
+})
