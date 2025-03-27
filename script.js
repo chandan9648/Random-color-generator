b01b8852c74d00e48b74da923b2d63330c06294d
Chandan Chaudhary <166887599+chandan9648@users.noreply.github.com>
2025-03-27 10:37:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,5 @@ btn.addEventListener('click', function(){
     let R = Math.floor(Math.random()*256) 
     let G = Math.floor(Math.random()*256)
     let B = Math.floor(Math.random()*256)
-
     box.style.backgroundColor = `rgb(${R},${G},${B})`
 })
