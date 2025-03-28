8455eb7943ebf582b16dc53c784dbf00ce1827c9
Chandan Chaudhary <166887599+chandan9648@users.noreply.github.com>
2025-03-28 23:10:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let btn = document.querySelector("button")
 let box = document.querySelector("#box")
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function(){ 
     let R = Math.floor(Math.random()*256) 
     let G = Math.floor(Math.random()*256)
     let B = Math.floor(Math.random()*256) 
